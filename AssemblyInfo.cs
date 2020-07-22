c3ca3eb2885741d5e9b4d830d7a791f2d726dc2c
Jason Davis <jasondavis303@gmail.com>
2020-07-21 23:44:15 -0600

    Update README.md

--- AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Test File")]
-[assembly: AssemblyVersion("1.2.3.4")]
-[assembly: AssemblyFileVersion("1.2.3.4")]
