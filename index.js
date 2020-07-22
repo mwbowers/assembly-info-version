5f601aca485705b3b1ededa809283a34c2384125
Jason Davis <jasondavis303@gmail.com>
2020-07-21 23:03:52 -0600

    try again

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 const fs = require('fs');
 const os = require("os");
 
-// most @actions toolkit packages have async methods
-async function run()
+function run()
 {
-    try
-    {
-        aip = process.env.AI_PATH;
+    aip = process.env.AI_PATH;
 
-        if (!fs.existsSync(aip))
-            throw new Error("AssemblyInfo file not found");
+    if (!fs.existsSync(aip))
+        throw new Error("AssemblyInfo file not found");
 
-        console.log(`AssemblyInfo Path: ${aip}`)
+    console.log(`AssemblyInfo Path: ${aip}`)
 
-        rgx = new RegExp("\[assembly: AssemblyVersion\(\"(.*)\"\)\]", "m")
+    rgx = new RegExp("\[assembly: AssemblyVersion\(\"(.*)\"\)\]", "m")
 
-        ver = this.rgx.exec(fs.readFileSync(aip, { encoding: "utf-8" }))[1];
+    ver = this.rgx.exec(fs.readFileSync(aip, { encoding: "utf-8" }))[1];
 
-        if (!ver)
-            throw new Error("Failed to get Assembly Version");
+    if (!ver)
+        throw new Error("Failed to get Assembly Version");
 
-        console.log(`Assembly Version: ${ver}`)
+    console.log(`Assembly Version: ${ver}`)
 
-        process.stdout.write(`::set-output name=ASSEMBLY_VERSION::${ver}` + os.EOL)
+    process.stdout.write(`::set-output name=ASSEMBLY_VERSION::${ver}` + os.EOL)
 
-    }
-    catch (error)
-    {
-        core.setFailed(error.message);
-    }
 }
 
-run();
+new Action().run()
